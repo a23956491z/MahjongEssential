e6081250707101e2bf4594802e602f834822d3e4
Easonchen0008 <beasonchen0008@gmail.com>
2020-08-10 04:45:42 +0800

    陳毅的計算台數app第一版 計算麻將台數UI之後修正 計算台數的按鍵大致上完成 可計算基本台數與歸零

---
+++ CalculationAPP/app/src/main/java/com/example/calculationapp/MyViewModel.java
@@ -0,0 +1,12 @@
+package com.example.calculationapp;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+
+public class MyViewModel extends ViewModel{
+    public int Score = 0;
+
+
+
+}
