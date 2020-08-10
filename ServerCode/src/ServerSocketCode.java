52fb23259d5ece6500ac40d80c0d6e29ab9e59a5
UXuan9686 <chen90313@gmail.com>
2020-08-10 18:05:00 +0800

    一份socket關不掉的code

---
+++ ServerCode/src/ServerSocketCode.java
@@ -0,0 +1,34 @@
+import java.net.*;
+import java.io.*;
+
+public class ServerSocketCode
+{
+	public static void main (String[] args) throws IOException
+	{
+		ServerSocket ss = new ServerSocket(8080);
+
+		while (true)
+		{
+			Socket cs = ss.accept();
+
+			new Thread( new Runnable() 
+			{
+				@Override
+
+				public void run()
+				{
+					try
+					{
+						cs.close();
+					}
+					catch(Exception e){e.printStackTrace();}
+				}
+
+			}).start();
+
+		}
+		
+		ss.close();
+	}
+	
+}
