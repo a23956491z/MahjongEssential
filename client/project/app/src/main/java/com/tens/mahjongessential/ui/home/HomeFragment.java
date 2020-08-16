1d460c49b707c44e32127b9e234adb1685529639
Easonchen0008 <beasonchen0008@gmail.com>
2020-08-17 03:40:16 +0800

    把博宇的APP加上骰子跟計算台By陳毅

--- client/project/app/src/main/java/com/tens/mahjongessential/ui/home/HomeFragment.java
+++ client/project/app/src/main/java/com/tens/mahjongessential/ui/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,17 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.tens.mahjongessential.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    Button button1;
+    TextView textView1;
+    @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,5 +37,29 @@ public class HomeFragment extends Fragment {
             }
         });*/
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        button1 =getView().findViewById(R.id.dice_button);
+        textView1 =getView().findViewById(R.id.home_number);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int i=0;
+                i = (int)(Math.random()*16+3);
+                textView1.setText(i+" ");
+            }
+        });
+        getView().findViewById(R.id.tsumoButton1).
+                setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_calculationFragment));
+        getView().findViewById(R.id.tsumoButton2).
+                setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_calculationFragment));
+        getView().findViewById(R.id.tsumoButton3).
+                setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_calculationFragment));
+        getView().findViewById(R.id.tsumoButton4).
+                setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_navigation_home_to_calculationFragment));
     }
 }
