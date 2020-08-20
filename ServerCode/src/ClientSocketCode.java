6f9f33562b52009b2d1ca3a178969eccb9dbe003
Enip <a23956491z@yahoo.com.tw>
2020-08-21 04:20:03 +0800

    將Server的Code回復到昱軒版本

--- ServerCode/src/ClientSocketCode.java
+++ ServerCode/src/ClientSocketCode.java
@@ -1,73 +1,26 @@
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import java.util.regex.Pattern;
 
 public class ClientSocketCode
 {
-
 	public static void main(String[] args)
     {
-    	startClient(8080);
-
+        try
+        {
+        	Socket cs = new Socket("127.0.0.1", 8080);//連線IP之後得更改，現在是本機連線
+        	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(cs.getOutputStream()));
+        	BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+        	while(true)
+        	{
+        		String str = reader.readLine();
+        		writer.write(str);
+        		writer.write("\n");
+        		writer.flush();
+        	}
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
-
-    public static void startClient(int port){
-
-		try(Socket sc = new Socket())
-		{
-
-			int timeout = 2000;
-			InetAddress address = InetAddress.getLocalHost();
-			SocketAddress sc_new_connect = new InetSocketAddress(address, port);
-			System.out.println("Connecting");
-
-			sc.connect(sc_new_connect, timeout);
-
-			DataInputStream in = new DataInputStream(sc.getInputStream());
-			DataOutputStream out = new DataOutputStream(sc.getOutputStream());
-
-			System.out.println(sc.getLocalAddress()+ " connected to " + sc.getInetAddress());
-
-			while(true) {
-
-				var scanner = new Scanner(System.in);
-				System.out.print("account : ");
-				String account = scanner.nextLine();
-				System.out.print("password : ");
-				String password = scanner.nextLine();
-
-				String account_password_format = "!" + account + "+" + password;
-				out.writeUTF(account_password_format);
-				out.flush();
-
-				String response = in.readUTF();
-
-				var pattern = Pattern.compile("\\!([01])");
-				var matcher = pattern.matcher(response);
-
-
-				if(matcher.find()){
-					boolean if_authenticated = matcher.group(1).equals("1") ? true : false;
-
-					if(if_authenticated) {
-						System.out.println("Login successful!");
-						break;
-					}else{
-						System.out.println("Login Failed!");
-					}
-				}
-			}
-
-
-		}catch(SocketTimeoutException e){
-			System.out.println("Timeout!");
-		}
-		catch(IOException e){
-			System.out.println("wtf here");
-			e.printStackTrace();
-		}
-	}
-
 }
--- ServerCode/src/ServerSocketCode.java
+++ ServerCode/src/ServerSocketCode.java
@@ -1,74 +1,39 @@
 import java.net.*;
 import java.io.*;
-import java.util.StringTokenizer;
-import java.util.regex.*;
 
-public class ServerSocketCode implements Runnable
+public class ServerSocketCode
 {
-	private int port;
-	private ServerSocket sc;
-
-
 	public static void main(String[] args)throws IOException
 	{
-		Thread socket_thread = new Thread(new ServerSocketCode(8080));
-		socket_thread.start();
-	}
-	public ServerSocketCode(int port) throws IOException{
-		this.port  = port;
-		this.sc = new ServerSocket(port);
-	}
-
-	public void run(){
-
-		while(true){
-			System.out.println("\nwaiting connection...");
-			try(Socket connection = this.sc.accept()){
-
-
-				while(true) {
-
-					System.out.println("\n"+connection.getInetAddress());
-					DataInputStream in = new DataInputStream(connection.getInputStream());
-
-					String in_str = in.readUTF();
-
-					System.out.println("\tget : " + in_str);
-
-					var pattern = Pattern.compile("\\!(\\w+)\\+(\\w+)");
-					var matcher = pattern.matcher(in_str);
-
-					if (matcher.find()) {
-						String account = matcher.group(1);
-						String password = matcher.group(2);
-
-						DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-
-						String out_str;
-						if (account.equals("test_acc_1") &&
-								password.equals("30678")) {
-
-
-							out_str = "!1";
-						} else {
-
-							out_str = "!0";
-						}
-
-						System.out.println("\treturn : " + out_str);
-						out.writeUTF(out_str);
-						out.flush();
-					}
-
-				}
-			}catch(SocketException | EOFException e){
-				System.out.println("\tconnection breaked!");
-
-			}catch(IOException e){
-				e.printStackTrace();
-
-			}
+		ServerSocket ss = new ServerSocket(8080);
+    
+		while(true)
+		{
+			System.out.println("waiting connect......");
+			Socket cs= ss.accept();
+			System.out.println("connect accept¡I");
+        
+			new Thread(new Runnable()
+			{
+				@Override
+			    public void run()
+			    {
+					BufferedReader reader = null;            
+			        try
+			        {
+			            //ss.close();
+			            reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+			            String str=reader.readLine();
+			            System.out.println("Client: " + str);
+			            
+			            cs.close();
+			        }
+			        catch (IOException e)
+			        {
+			            e.printStackTrace();
+			        }
+			    }
+			}).start();						
 		}
 	}
-
 }
