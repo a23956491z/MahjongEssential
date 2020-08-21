86dcfa1cd23714f0a457f7150f0e30be230728c2
Enip <a23956491z@yahoo.com.tw>
2020-08-22 02:39:47 +0800

    server_Socket已支援多執行緒
    可同時多個client連入並傳輸資料
    每次傳輸資料時會附帶辨識碼(identityCode)以供確認為不同實例

--- Server/src/main/java/client_socket.java
+++ Server/src/main/java/client_socket.java
@@ -1,26 +1,59 @@
 import java.net.*;
 import java.io.*;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class client_socket
 {
+	private static Integer identityCode;
+
 	public static void main(String[] args)
     {
-        try
-        {
-        	Socket cs = new Socket("127.0.0.1", 8080);
-        	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(cs.getOutputStream()));
-        	BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-        	while(true)
-        	{
-        		String str = reader.readLine();
-        		writer.write(str);
-        		writer.write("\n");
-        		writer.flush();
-        	}
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
+    	startClient(8080);
+	}
+
+    public static void startClient(int port){
+
+		try(Socket sc = new Socket())
+		{
+
+			int timeout = 2000;
+			InetAddress address = InetAddress.getLocalHost();
+			SocketAddress sc_new_connect = new InetSocketAddress(address, port);
+			System.out.println("Connecting");
+
+			sc.connect(sc_new_connect, timeout);
+
+			DataInputStream in = new DataInputStream(sc.getInputStream());
+			DataOutputStream out = new DataOutputStream(sc.getOutputStream());
+
+			identityCode = new Date().hashCode() + sc.getLocalAddress().hashCode();
+
+			System.out.println(InetAddress.getLocalHost()+ " connected to " + sc.getInetAddress());
+			System.out.println("IdentityCode is " + identityCode);
+			while(true){
+
+				Scanner scanner = new Scanner(System.in);
+
+				out.writeUTF(identityCode.toString());
+
+				String str_in = scanner.nextLine();
+				out.writeUTF(str_in);
+				out.flush();
+
+			}
+
+
+
+		}catch(SocketTimeoutException e){
+			System.out.println("Timeout!");
+		}
+		catch(IOException e){
+			System.out.println("wtf here");
+			e.printStackTrace();
+		}
+	}
+
 }
--- Server/src/main/java/server_socket.java
+++ Server/src/main/java/server_socket.java
@@ -1,39 +1,100 @@
 import java.net.*;
 import java.io.*;
+import java.util.StringTokenizer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.regex.*;
 
 public class server_socket
 {
+	final private static int port = 8080;
+	private ServerSocket sc;
+
+
 	public static void main(String[] args)throws IOException
 	{
-		ServerSocket ss = new ServerSocket(8080);
-    
-		while(true)
-		{
-			System.out.println("waiting connect......");
-			Socket cs= ss.accept();
-			System.out.println("connect accept¡I");
-        
-			new Thread(new Runnable()
-			{
-				@Override
-			    public void run()
-			    {
-					BufferedReader reader = null;            
-			        try
-			        {
-			            //ss.close();
-			            reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-			            String str=reader.readLine();
-			            System.out.println("Client: " + str);
-			            
-			            cs.close();
-			        }
-			        catch (IOException e)
-			        {
-			            e.printStackTrace();
-			        }
-			    }
-			}).start();						
+
+		new server_socket().startServer();
+	}
+
+	public void startServer(){
+		final ExecutorService clientProcessingPool = Executors.newFixedThreadPool(10);
+
+		Runnable serverTask = new Runnable() {
+			@Override
+			public void run() {
+				try{
+					ServerSocket serverSocket = new ServerSocket(port);
+					System.out.println("Waiting connection...");
+
+					while(true){
+						Socket clientSocket = serverSocket.accept();
+						clientProcessingPool.submit(new ClientTask(clientSocket));
+					}
+
+				}catch(BindException e){
+					System.err.println("This connection port already in use \n" +
+										"\tuse another port or stop the existing server!");
+					e.printStackTrace();
+				} catch(IOException e){
+					System.err.println("Unable to process client request");
+					e.printStackTrace();
+				}
+			}
+		};
+
+		Thread serverThread = new Thread(serverTask);
+		serverThread.start();
+
+
+	}
+
+	private class ClientTask implements Runnable{
+		private final Socket clientSocket;
+		private String identityCode;
+		private InetAddress address;
+
+
+		private ClientTask(Socket clientSocket){
+
+			this.clientSocket = clientSocket;
+			address =  clientSocket.getInetAddress();
+		}
+
+		@Override
+		public void run(){
+
+			try {
+
+				DataInputStream in = new DataInputStream(clientSocket.getInputStream());
+				DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+
+				while(true) {
+
+					String identityCode = in.readUTF();
+
+					String in_str = in.readUTF();
+					System.out.println("\n" + clientSocket.getInetAddress() +" as identityCode : "+ identityCode);
+
+					System.out.println("\tget : " + in_str);
+				}
+			}catch(SocketException e) {
+				System.out.println("A connection break from "+ address +" !!");
+			}
+			catch(IOException e){
+
+				e.printStackTrace();
+			}
+
+
+
+
+			try{
+				clientSocket.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
 		}
 	}
+
 }
