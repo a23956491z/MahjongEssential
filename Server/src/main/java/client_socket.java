591835506268ff16f7cdf9351f4bccc9f41a22aa
UXuan9686 <chen90313@gmail.com>
2020-08-23 19:11:14 +0800

    改動了socket的開啟方式 client新增的功能有回傳問題

--- Server/src/main/java/client_socket.java
+++ Server/src/main/java/client_socket.java
@@ -4,23 +4,45 @@ import java.io.*;
 public class client_socket
 {
 	public static void main(String[] args)
-    {
-        try
+	{
+		ClientLogin("abc123","123123");
+	
+	}
+	public static boolean ClientLogin( String account, String password)
+	{
+		try(Socket cs = new Socket("127.0.0.1", 8080))
         {
-        	Socket cs = new Socket("127.0.0.1", 8080);
-        	BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(cs.getOutputStream()));
-        	BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-        	while(true)
-        	{
-        		String str = reader.readLine();
-        		writer.write(str);
-        		writer.write("\n");
-        		writer.flush();
-        	}
-        }
+        	int timeout = 2000;
+        	SocketAddress sa = new InetSocketAddress("127.0.0.1", 8080);
+        	System.out.println("waiting connect......");
+        	cs.connect(sa, timeout);
+        	
+        	DataInputStream in = new DataInputStream(cs.getInputStream());
+			DataOutputStream out = new DataOutputStream(cs.getOutputStream());
+
+			String act = new String("@1");
+			out.writeUTF(account);
+			out.writeUTF(password);
+			out.writeUTF(act);
+			
+			String result = in.readUTF();
+			switch(result)
+			{
+				case "1":
+					System.out.println("Login Successful");
+					return true;
+				default:
+					System.out.println("Login fail");
+					return false;
+			}
+			
+        }catch(SocketTimeoutException e)
+        {
+			System.out.println("Timeout");
+		}
         catch (IOException e)
         {
             e.printStackTrace();
         }
-    }
+	}
 }
--- Server/src/main/java/server_socket.java
+++ Server/src/main/java/server_socket.java
@@ -1,39 +1,78 @@
 import java.net.*;
 import java.io.*;
+import java.sql.SQLException;
+import practice.enip.sql.Account;
+
 
 public class server_socket
 {
 	public static void main(String[] args)throws IOException
 	{
-		ServerSocket ss = new ServerSocket(8080);
-    
-		while(true)
+		try( ServerSocket ss = new ServerSocket(8080))
 		{
-			System.out.println("waiting connect......");
-			Socket cs= ss.accept();
-			System.out.println("connect accept¡I");
-        
-			new Thread(new Runnable()
+			while(true)
 			{
-				@Override
-			    public void run()
-			    {
-					BufferedReader reader = null;            
-			        try
-			        {
-			            //ss.close();
-			            reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-			            String str=reader.readLine();
-			            System.out.println("Client: " + str);
-			            
-			            cs.close();
-			        }
-			        catch (IOException e)
-			        {
-			            e.printStackTrace();
-			        }
-			    }
-			}).start();						
+				System.out.println("waiting connect......");
+				Socket cs= ss.accept();
+				System.out.println("connect accept¡I");
+        
+				new Thread(new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						try
+						{
+							DataInputStream in = new DataInputStream(cs.getInputStream());
+							DataOutputStream out = new DataOutputStream(cs.getOutputStream());
+							
+							String account = in.readUTF();
+							String password = in.readUTF();
+							String act = in.readUTF();
+							
+							try(Account acc = new Account())
+							{
+								Integer login_status = acc.authenticate(account, password);
+
+								switch(act)
+								{
+									case "@1":
+
+										if(login_status.equals(1)){
+											System.out.println("\t There is a Valid Login for " + account);
+										}else{
+											System.out.println("\t There is a FAILED Login for " + account);
+										}
+
+										out.writeUTF(login_status.toString());
+										break;
+									default:
+										System.err.println("No matching behaviour!!!!");
+								}
+							}
+							catch(SQLException e)
+							{
+								e.printStackTrace();
+							}
+
+							
+							cs.close();
+						}
+						catch (IOException e)
+						{
+							e.printStackTrace();
+						}
+					}
+				}).start();					
+			}
+		}catch(SocketException e) 
+		{
+			e.printStackTrace();
 		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
 	}
 }
